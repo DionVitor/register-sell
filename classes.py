bd7586e7e1369c6193773682cf1ca1effba51759
DionVitor <dionvictor11@gmail.com>
2020-10-18 09:58:15 -0300

    Add new feature.
    Now the program have a new feature: Excluir dívida.

--- classes.py
+++ classes.py
@@ -5,7 +5,6 @@ class Action:
     def __init__(self, option):
         if option == '1':
             lines()
-
             nome = str(input('Digite o nome do comprador: '))
             produto = str(input('Digite qual produto você vendeu: '))
             valor = input('Digite o preço da venda: ')
@@ -67,3 +66,41 @@ class Action:
                 total += float(line[2])
             a.close()
             print(f'O valor total de dívidas é: {total}')
+
+        if option == '4':
+            list_of_clients = []
+            lines()
+            a = open(archive)
+            for c in range(0, lines_in_archive(archive)):
+                line = a.readline().replace('\n', '').split('/')
+                if line[0] not in list_of_clients:
+                    print(f'Cliente: {line[0]}')
+                    list_of_clients.append(line[0])
+                    lines()
+            a.close()
+
+            remove = str(input('Qual cliente você deseja excluir a dívida?')).title()
+            debt = 0
+            list_with_outhers_clients = []
+            a = open(archive)
+            for c in range(0, lines_in_archive(archive)):
+                line = a.readline().replace('\n', '').split('/')
+                if line[0] == remove:
+                    debt += float(line[2])
+                else:
+                    list_with_outhers_clients.append(line)
+
+            if debt != 0:
+                print(f'A dívida atual do cliente {remove} é: {debt}')
+            else:
+                print(f'Cliente {remove} não encontrado.')
+            a.close()
+            confirmation_remove_debt = str(input(f'Você realmente deseja excluir a dívida de {remove} no valor de {debt} reias? [S/N]'))
+            if confirmation_remove_debt in 'Ss':
+                a = open(archive, 'w')
+                for c in range(0, len(list_with_outhers_clients)):
+                    a.write(f'{list_with_outhers_clients[c][0]}/{list_with_outhers_clients[c][1]}/{list_with_outhers_clients[c][2]}\n')
+                a.close()
+                print(f'Foi exluído a dívida de {remove}.')
+            else:
+                print(f'Não foi excluído a dívida de {remove}.')

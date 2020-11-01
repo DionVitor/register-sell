00bac23fffb92e27f2a70a1fdbdbb43abcd5c42f
Dion Vitor do Prado <71671413+DionVitor@users.noreply.github.com>
2020-10-31 21:45:29 -0300

    Update GUI.py
    Chance PySimpleGUI for Kivy.

--- GUI.py
+++ GUI.py
@@ -1,28 +1,44 @@
-import PySimpleGUI as sg
-from actions import Action
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
-sg.theme('Reddit')
-layout = [
-    [sg.Text('1 - Cadastrar dívida')],
-    [sg.Text('2 - Buscar dívida')],
-    [sg.Text('3 - Todos os devedores')],
-    [sg.Text('4 - Total de dívidas')],
-    [sg.Text('5 - Diminuir uma dívida')],
-    [sg.Text('6 - Excluir dados')],
-    [sg.Text('7 - Fechar programa')],
-    [sg.Text('Digite uma opção:'), sg.Input(key='option', size=(15, 0)), sg.Button('OK'), sg.Button('Sair')]
-]
 
-window = sg.Window('Cadastramento de dívidas', layout)
-while True:
-    eventos, valores = window.read()
+class MyMenuLayout(BoxLayout):
+    def __init__(self, **kwargs):
+        super(MyMenuLayout, self).__init__(**kwargs)
+        self.orientation = 'vertical'
 
-    print(eventos)
-    print(valores)
+        self.add_widget(Label(text='MENU', font_size=40))
 
-    if eventos == sg.WIN_CLOSED or eventos == 'Sair':
-        break
-    if valores['option'] not in '1234567':
-        sg.popup('Erro!', 'Digite uma opção válida!')
-    else:
-        Action(valores['option'])
+        self.add_widget(Button(text='Cadastrar dívida'))
+
+        self.add_widget(Button(text='Buscar dívida'))
+
+        self.add_widget(Button(text='Todos os devedores'))
+
+        self.add_widget(Button(text='Total de dívidas'))
+
+        self.add_widget(Button(text='Diminuir uma dívida'))
+
+        self.add_widget(Button(text='Excluir dados'))
+
+
+class MyLayout(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyLayout, self).__init__(**kwargs)
+        self.cols = 2
+        self.padding = 10
+
+        self.add_widget(MyMenuLayout(size_hint=(0.4, 1)))
+        self.add_widget(Label())
+
+
+class System(App):
+    def build(self):
+        return MyLayout()
+
+
+if __name__ == '__main__':
+    System().run()
